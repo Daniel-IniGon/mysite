78a2def72b8e7ea34f4ca4dbfe85e4221f3bc24c
Daniel Iniesta Gonzalez <niraen.megar@gmail.com>
2021-10-29 23:50:45 +0200

    Gifs reproducer - kanjipractice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 "use strict";
 
 //Enable tooltips everywhere
+/*
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+*/
 
-
-/* Vanilla RSS - https://github.com/sdepold/vanilla-rss */
+/* Vanilla RSS - https://github.com/sdepold/vanilla-rss 
 
 	const rss = new RSS(
 	    document.querySelector("#rss-feeds"),
@@ -38,11 +39,12 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	    }
 	);
 	rss.render();
+*/
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
+    //new GitHubCalendar("#github-graph", "IonicaBizau", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    //GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
